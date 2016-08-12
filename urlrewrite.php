dbd81b651ba00c07735abd937c4624d0432f6c05
Admin <git@webgk.ru>
2016-08-12 10:46:17 +0300

    add news component on services page

--- urlrewrite.php
+++ urlrewrite.php
@@ -7,6 +7,12 @@ $arUrlRewrite = array(
 		"PATH" => "/portfolio/index.php",
 	),
 	array(
+		"CONDITION" => "#^/services/#",
+		"RULE" => "",
+		"ID" => "bitrix:news",
+		"PATH" => "/services/index.php",
+	),
+	array(
 		"CONDITION" => "#^/news/#",
 		"RULE" => "",
 		"ID" => "bitrix:news",
